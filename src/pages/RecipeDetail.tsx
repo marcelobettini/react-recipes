4a510accc1a31d95405bf3f603cc42c5098b4f67
Marcelo Bettini <bettinimarcelo@gmail.com>
2025-08-05 22:52:08 -0300

    minor adjustments

--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -1,73 +1,54 @@
-import { useState, useEffect } from 'react'
 import './RecipeDetail.css'
-import { useParams, useNavigate } from 'react-router'
+import { useParams } from 'react-router'
 import type { Recipe } from '../types'
+import { useEffect, useState } from 'react'
 export default function RecipeDetail() {
-    const navigate = useNavigate()
     const { id } = useParams<{ id: string }>()
-    const [recipe, setRecipe] = useState<Recipe | null>(null)
-    const [error, setError] = useState<string | null>(null)
-
+    const [recipe, setRecipe] = useState<Recipe>()
     useEffect(() => {
-        fetch(`https://67f95738094de2fe6ea13bdf.mockapi.io/api/v1/recipes/${id}`)
-            .then(response => {
-                if (!response.ok) {
-                    setError(`Error fetching recipe with id ${id}: ${response.statusText}`)
-                }
-                return response.json()
-            })
-            .then(data => {
-                console.log(data)
-                setRecipe(data)
-            })
-            .catch((error) => setError(error.message))
-    }, [id])
-
-    if (!recipe) {
-        return <p>Loading recipe...</p>
-    }
-    if (error) {
-        return (
-            <div className="error">
-                <p>Error loading recipe: {error}</p>
-                <button onClick={() => navigate(-1)}>Back</button>
-            </div>
-        )
-    }
+        fetch('https://67f95738094de2fe6ea13bdf.mockapi.io/api/v1/recipes/' + id)
+            .then(response => response.json())
+            .then(data => setRecipe(data))
+            .catch(error => console.error('Error fetching recipes:', error))
+    }, [])
     return (
         <>
 
+            {recipe ? (
+                <div>
+                    <p style={{ marginBottom: 0, fontStyle: 'italic', color: 'hotpink' }}>{recipe.mealType.join(", ")}</p>
+                    <h2 style={{ marginTop: 0, color: 'pink' }}>{recipe.name}</h2>
+                    <span>{recipe.cuisine} cuisine</span>
+                    <hr style={{ marginTop: 0, border: 'none', height: '1px', backgroundColor: 'gray' }} />
+                    <p>Rating: {recipe.rating} ★</p>
+                    <p>Difficulty: {recipe.difficulty}</p>
+                    <p>Preparation Time: {recipe.prepTimeMinutes} ⌛️</p>
+                    <p>Cooking Time: {recipe.cookTimeMinutes} ⏰</p>
+                    <p>Servings: {recipe.servings} 🍽️</p>
+                    {/* style img tag with aspect ratio to avoid layout shifting */}
+                    <img src={recipe.image} alt={recipe.name} className='img-detail'
+                        loading='eager'
+                    />
+                    <hr style={{ border: 'none', height: '1px', backgroundColor: 'gray' }} />
+                    <h3>Ingredients:</h3>
+                    <ul>
+                        {recipe.ingredients?.map((ingredient, index) => (
+                            <li key={index}>{ingredient}</li>
+                        ))}
+                    </ul>
+                    <hr style={{ border: 'none', height: '1px', backgroundColor: 'gray' }} />
+                    <h3>Instructions:</h3>
+                    <ul>
+                        {recipe.instructions?.map((step, index) => (
+                            <li key={index}>{step}</li>
+                        ))}
+                    </ul>
 
-            <div>
-                <p style={{ marginBottom: 0, fontStyle: 'italic', color: 'hotpink' }}>{recipe.mealType.join(", ")}</p>
-                <h2 style={{ marginTop: 0, color: 'pink' }}>{recipe.name}</h2>
-                <span>{recipe.cuisine} cuisine</span>
-                <hr style={{ marginTop: 0, border: 'none', height: '1px', backgroundColor: 'gray' }} />
-                <p>Rating: {recipe.rating} ★</p>
-                <p>Difficulty: {recipe.difficulty}</p>
-                <p>Preparation Time: {recipe.prepTimeMinutes} ⌛️</p>
-                <p>Cooking Time: {recipe.cookTimeMinutes} ⏰</p>
-                <p>Servings: {recipe.servings} 🍽️</p>
-                {/* style img tag with aspect ratio to avoid layout shifting */}
-                <img src={recipe.image} alt={recipe.name} className='img-detail' />
-                <hr style={{ border: 'none', height: '1px', backgroundColor: 'gray' }} />
-                <h3>Ingredients:</h3>
-                <ul>
-                    {recipe.ingredients?.map((ingredient, index) => (
-                        <li key={index}>{ingredient}</li>
-                    ))}
-                </ul>
-                <hr style={{ border: 'none', height: '1px', backgroundColor: 'gray' }} />
-                <h3>Instructions:</h3>
-                <ul>
-                    {recipe.instructions?.map((step, index) => (
-                        <li key={index}>{step}</li>
-                    ))}
-                </ul>
 
-
-            </div>
-
+                </div>
+            ) : (
+                <p>Recipe not found</p>
+            )}
         </>
     )
 }
